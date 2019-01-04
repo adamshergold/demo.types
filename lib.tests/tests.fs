9ee24652f42b3e5d6cf8bf96b0683cd671216e6e
Adam Shergold <mail@adamshergold.co.uk>
2019-01-04 18:00:56 +0000

    update

---
+++ lib.tests/tests.fs
@@ -0,0 +1,45 @@
+namespace Demo.Types.Lib.Tests
+
+open Microsoft.Extensions.Logging
+
+open Xunit
+open Xunit.Abstractions
+
+open Example.Serialisation
+
+open Demo.Types
+
+type LibShould( oh :ITestOutputHelper ) =
+    
+    let logger =
+        let options = { Logging.Options.Default with OutputHelper = Some oh }
+        Logging.CreateLogger options
+        
+    [<Theory>]
+    [<InlineData("json")>]
+    [<InlineData("binary")>]
+    member this.``RoundTripTheDemoType`` (contentType:string) =
+        
+        let serde =
+            Serde.Make( SerdeOptions.Default )
+            
+        let nSerialisersRegistered =
+            serde.TryRegisterAssembly typeof<Address>.Assembly 
+        
+        logger.LogInformation( "Registered {nSerialisersRegistered} serialisers", nSerialisersRegistered )
+        
+        let example =
+            Address.Make( 1, "High Street" )
+
+        let bytes =
+            example |> Helpers.Serialise serde (Some contentType)
+            
+        if contentType = "json" then
+            logger.LogInformation( "{JSON}", bytes |> System.Text.Encoding.UTF8.GetString )
+        else
+            logger.LogInformation( "{Bytes}", bytes )
+            
+        let example' =
+            bytes |> Helpers.DeserialiseT<_> serde (Some contentType)
+            
+        Assert.Equal( example', example )
